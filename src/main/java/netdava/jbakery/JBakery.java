c53dd262427389266d9d378a573d7fad6e887f78
Ioan Eugen Stan <eu@ieugen.ro>
2015-11-05 00:34:47 +0200

    JBakery initial import

---
+++ src/main/java/netdava/jbakery/JBakery.java
@@ -0,0 +1,12 @@
+package netdava.jbakery;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JBakery {
+
+    public static void main(String[] args) {
+        log.info("LLogging from JBakery");
+    }
+
+}

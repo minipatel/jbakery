9910f24203ac930e651cb2f22ec064455b758ce6
Ioan Eugen Stan <eu@ieugen.ro>
2015-11-05 23:16:46 +0200

    Basic web-app setup
    * thymeleaf templates
    * apache log4j2 logging
    * bower for web components management
    * simple layouts

--- src/main/java/netdava/jbakery/JBakery.java
+++ src/main/java/netdava/jbakery/JBakery.java
@@ -1,12 +1,40 @@
 package netdava.jbakery;
 
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+
+import java.io.File;
 
 @Slf4j
 public class JBakery {
 
     public static void main(String[] args) {
-        log.info("LLogging from JBakery");
+        System.setProperty("user.timezone", "UTC");
+        setVertxLogger();
+        conventionBasedLoggingConfig();
+
+        log.info("Logging from JBakery");
+
+        DeploymentOptions options = new DeploymentOptions();
+
+        final Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new JBakeryVerticle(), options);
+    }
+
+    private static void conventionBasedLoggingConfig() {
+        //TODO: move path in config
+        LoggerContext log4jContext = (LoggerContext) LogManager.getContext(false);
+        File file = new File("conf/log4j2.xml");
+        // this will force a reconfiguration
+        log4jContext.setConfigLocation(file.toURI());
+    }
+
+    private static void setVertxLogger() {
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+        log.info("Logging with " + System.getProperty("vertx.logger-delegate-factory-class-name"));
     }
 
 }
